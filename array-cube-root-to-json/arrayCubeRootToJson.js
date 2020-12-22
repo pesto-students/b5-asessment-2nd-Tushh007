00716275b64372de1af441225a61bfe8077a44c5
Tushar Langer <tushar.langer@joulon.com>
2020-12-22 15:20:30 +0530

    finished 2 questions out of 3

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,16 @@
-const arrayCubeRootToJson = arr => {};
+const arrayCubeRootToJson = (arr) => {
+  if (typeof arr !== "object") throw "invalid input";
 
+  let result = {};
+  arr.forEach((element) => {
+    if (typeof element === "boolean" || isNaN(element))
+      throw `invalid element found ${element}`;
+      
+    result[element] = Math.cbrt(element);
+  });
+
+  return result;
+};
+
+// arrayCubeRootToJson.test
 export { arrayCubeRootToJson };
